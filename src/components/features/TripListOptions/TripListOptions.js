90633bc184bd6e3293e4be203b2ad54cd48e5b4e
Małgorzata Jańczak <m.janczak@int.pl>
2019-11-01 16:20:32 +0100

    add filters state change

--- src/components/features/TripListOptions/TripListOptions.js
+++ src/components/features/TripListOptions/TripListOptions.js
@@ -6,18 +6,19 @@ import {Row, Col} from 'react-flexbox-grid';
 
 class TripListOptions extends React.Component {
   handleTags(tag, checked){
+    const tagsArray = this.props.filters.tags;
     if(checked) {
-      console.log('Adding tag', tag);
-      // TODO - use action dispatcher from props
+      tagsArray.push(tag);
+      this.props.addTag(tagsArray);
     } else {
-      console.log('Removing tag', tag);
-      // TODO - use action dispatcher from props
+      const tagIndex = tagsArray.indexOf(tag);
+      tagsArray.splice(tagIndex, 1);
+      this.props.removeTag(tagsArray);
     }
   }
 
   handleDuration(type, value){
-    console.log('Changing duration', type, value);
-    // TODO - use action dispatcher from props
+    this.props.changeDuration(type, parseInt(value), this.props.filters.duration.from, this.props.filters.duration.to);
   }
 
   handleSearch(phrase){
@@ -73,6 +74,9 @@ TripListOptions.propTypes = {
   tags: PropTypes.object,
   filters: PropTypes.object,
   changeSearchPhrase: PropTypes.func,
+  addTag: PropTypes.func,
+  removeTag: PropTypes.func,
+  changeDuration: PropTypes.func,
 };
 
 export default TripListOptions;
--- src/components/features/TripListOptions/TripListOptionsContainer.js
+++ src/components/features/TripListOptions/TripListOptionsContainer.js
@@ -1,7 +1,7 @@
 import {connect} from 'react-redux';
 import TripListOptions from './TripListOptions';
 import {getAllTags} from '../../../redux/tagsRedux';
-import {getAllFilters, changeSearchPhrase} from '../../../redux/filtersRedux';
+import {getAllFilters, changeSearchPhrase, changeDuration, addTag, removeTag} from '../../../redux/filtersRedux';
 
 const mapStateToProps = state => ({
   tags: getAllTags(state),
@@ -11,6 +11,16 @@ const mapStateToProps = state => ({
 const mapDispatchToProps = dispatch => ({
   changeSearchPhrase: phrase => dispatch(changeSearchPhrase(phrase)),
   // TODO - add more dispatchers for other filters
+  changeDuration: (type, value, fromVal, toVal) => {
+    if(type === 'from') {
+      dispatch(changeDuration({[type]: value, to: toVal}));
+    }
+    else if(type === 'to') {
+      dispatch(changeDuration({from: fromVal, [type]: value}));
+    }
+  },
+  addTag: (tagsArray) => dispatch(addTag(tagsArray)),
+  removeTag: (tagsArray) => dispatch(removeTag(tagsArray)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(TripListOptions);
