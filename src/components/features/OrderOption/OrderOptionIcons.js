fe2c0821f9610a5322e88b08c5edd28fa1f2c514
Małgorzata Jańczak <m.janczak@int.pl>
2019-11-07 20:35:55 +0100

    add value field

--- src/components/features/OrderOption/OrderOptionIcons.js
+++ src/components/features/OrderOption/OrderOptionIcons.js
@@ -7,7 +7,7 @@ import {formatPrice} from '../../../utils/formatPrice.js';
 const OrderOptionIcons = ({values, required, currentValue, setOptionValue}) => (
   <div className={styles.component}>
     {required ? '' : (
-      <div key='' onClick={setOptionValue('')}><Icon name='times-circle'/>none</div>
+      <div key='' onClick={() => setOptionValue('')}><Icon name='times-circle'/>none</div>
     )}
     {values.map(value => (
       <div className={value.id == currentValue ? (styles.icon, styles.iconActive) : styles.icon} key={value.id} onClick={() => setOptionValue(value.id)}><Icon name={value.icon}/>{value.name}({formatPrice(value.price)})</div>
--- src/components/features/OrderOption/OrderOptionNumber.js
+++ src/components/features/OrderOption/OrderOptionNumber.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './OrderOption.scss';
+import {formatPrice} from '../../../utils/formatPrice.js';
 
 
-const OrderOptionNumber = props => (
-  <div className={styles.component}>
-    {props.id}
+const OrderOptionNumber = ({currentValue, setOptionValue, limits, price}) => (
+  <div className={styles.number}>
+    <input type='number'
+      className={styles.inputSmall}
+      value={currentValue} min={limits.min} max={limits.max}
+      onChange={event => setOptionValue(event.currentTarget.value)} /> paid {formatPrice(price)}
   </div>
 );
 
 OrderOptionNumber.propTypes = {
   id: PropTypes.string,
   defaultValue: PropTypes.number,
-  price: PropTypes.string,
   limits: PropTypes.object,
+  price: PropTypes.string,
+  currentValue: PropTypes.number,
+  setOptionValue: PropTypes.func,
 };
 
 export default OrderOptionNumber;
