5e23e4e9b155bc83348251fb3538a6c2282380b2
Małgorzata Jańczak <m.janczak@int.pl>
2019-11-14 15:24:36 +0100

    add simple validation

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -5,16 +5,69 @@ import styles from './OrderForm.scss';
 import OrderSummary from '../OrderSummary/OrderSummary';
 import OrderOption from '../OrderOption/OrderOption';
 import {Row, Col} from 'react-flexbox-grid';
+import settings from '../../../data/settings';
+import {formatPrice} from '../../../utils/formatPrice';
+import {calculateTotal} from '../../../utils/calculateTotal';
+import Button from '../../common/Button/Button';
+
+const sendOrder = (options, tripCost, tripName, tripId, countryCode) => {
+  const totalCost = formatPrice(calculateTotal(tripCost, options));
+
+  const payload = {
+    ...options,
+    totalCost,
+    tripName,
+    tripId,
+    countryCode,
+  };
+
+  const url = settings.db.url + '/' + settings.db.endpoint.orders;
+
+  const fetchOptions = {
+    cache: 'no-cache',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  };
+
+  fetch(url, fetchOptions)
+    .then(function(response){
+      return response.json();
+    }).then(function(parsedResponse){
+      console.log('parsedResponse', parsedResponse);
+    });
+
+};
 
 const OrderForm = props => (
   <Row>
     {pricing.map(option => (
       <Col md={4} key={option.id} className={styles.formField}>
-        <OrderOption {...option} currentValue={props.options[option.id]} setOrderOption={props.setOrderOption}/>
+        <OrderOption {...option} currentValue={props.options[option.id]} setOrderOption={props.setOrderOption} />
       </Col>
     ))}
     <Col xs={12}>
       <OrderSummary totalCost={props.tripCost} tripOptions={props.options}/>
+    </Col>
+    <Col xs={12}>
+      <Button onClick={() => {
+        if(props.options.name == '' || props.options.contact == ''){
+          if(props.options.name == ''){
+            window.alert('Name can not be empty!');
+          }
+          if(props.options.contact == ''){
+            window.alert('Contact can not be empty!');
+          }
+        }
+        else {
+          sendOrder(props.options, props.tripCost, props.tripName, props.tripId, props.countryCode);
+          window.alert('Form submitted!');
+        }
+      }}>Order now!</Button>
+    </Col>
+    <Col xs={12}>
     </Col>
   </Row>
 );
@@ -23,6 +76,9 @@ OrderForm.propTypes = {
   tripCost: PropTypes.string,
   options: PropTypes.object,
   setOrderOption: PropTypes.func,
+  tripName: PropTypes.string,
+  tripId: PropTypes.string,
+  countryCode: PropTypes.string,
 };
 
 export default OrderForm;
